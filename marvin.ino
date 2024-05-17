07f5db0871e975a7eb1258bd1f0f9f3b3fcbe85d
raygeeknyc <raygeeknyc@gmail.com>
2024-05-17 12:01:17 -0400

    Added simple sweep Enhanced the LED pulsing functionality Tested, but not complete functionality

--- marvin.ino
+++ marvin.ino
@@ -20,17 +20,20 @@
 #define PIN_PING_ECHO 4
 
 // Servo declarations
-#define SERVO_MAX_POS 180
-#define SERVO_MIN_POS 0
+#define SERVO_POS_MAX 180
+#define SERVO_POS_MIN 0
 #define SERVO_MOVEMENT_DELAY_MS 15
 #define SERVO_SWEEP_STEP 5
+#define SWEEP_COUNT 2
+unsigned long int lastServoPointAt;
 Adafruit_SoftServo myServo;
 
 // LED declarations
-#define LED_MIN 0
-#define LED_MAX 255
+#define LED_MIN 20
+#define LED_MAX 200
 #define LED_STEP 5
-#define LED_PULSE_DUR_MS 40
+#define LED_PULSE_DUR_MS 15
+#define LONG_PULSE_DUR_MS 2000
 int LEDLevel;
 int LEDStep;
 unsigned long int lastLEDPulseAt;
@@ -39,24 +42,29 @@ unsigned long int lastLEDPulseAt;
 #define PING_SAMPLES 5
 #define MAX_DISTANCE_CM 200
 #define DISTANCE_DELTA_THRESHOLD_CM 8
-#define LOOP_MIN_TIME_MS 80
+#define LOOP_MIN_TIME_MS 5000
 #define LOOP_TIMER_STEP_MS 5
 int dist, prev_dist;
-const int DEFAULT_LOCATION = (SERVO_MAX_POS - SERVO_MIN_POS) / 2;
+const int DEFAULT_LOCATION = (SERVO_POS_MAX - SERVO_POS_MIN) / 2;
 int min_distance;  // min distance seen during a sweep()
 int min_location;  // Servo position at min distance reading during a sweep()
 NewPing sonar(PIN_PING_TRIG, PIN_PING_ECHO, MAX_DISTANCE_CM); // NewPing setup of pins and maximum distance.
 
 
-void startLEDPulsing() {
-  LEDLevel = LED_MIN;
-  LEDStep = LED_STEP;
+void startLEDPulsing(bool startHigh=false) {
+  if (startHigh) {
+    LEDLevel = LED_MAX;
+    LEDStep = LED_STEP*-1;
+  } else {
+    LEDLevel = LED_MIN;
+    LEDStep = LED_STEP;
+  }
   lastLEDPulseAt = 0;
 }
 
-void stopLEDPulsing() {
+void stopLEDPulsing(int offVal=0) {
   LEDLevel = LED_MIN;
-  analogWrite(PIN_LED, LEDLevel);
+  analogWrite(PIN_LED, offVal);
 }
 
 void refreshPulsingLED() {
@@ -85,40 +93,61 @@ int getDistanceCM() {
 }
 
 void pointAt(int pos) {
+  long remainingServoDelay = SERVO_MOVEMENT_DELAY_MS - (millis() - lastServoPointAt);
+  if (remainingServoDelay > 0) {
+    delay(remainingServoDelay);
+  }
   myServo.write(pos);
   myServo.refresh();
-  delay(SERVO_MOVEMENT_DELAY_MS);
+  lastServoPointAt = millis();
 }
 
 void setup() {
   long timerStart = millis();
+  lastServoPointAt = 0;
+
   // Pulse LED for 2 seconds
   startLEDPulsing();
-  while (millis() < timerStart+2000) {
-    refreshPulsingLED();
+  while (millis() < (timerStart+LONG_PULSE_DUR_MS)) {
+   refreshPulsingLED();
   }
+  stopLEDPulsing();
 
   myServo.attach(PIN_SERVO);
   // Sweep the servo back and forth
-  pointAt(SERVO_MIN_POS);
-  for (int i=SERVO_MIN_POS; i<=SERVO_MAX_POS; i+=SERVO_SWEEP_STEP) {
+  pointAt(SERVO_POS_MIN);
+  for (int i=SERVO_POS_MIN; i<=SERVO_POS_MAX; i+=SERVO_SWEEP_STEP) {
     pointAt(i);
   }
-  for (int i=SERVO_MAX_POS; i>=SERVO_MIN_POS; i-=SERVO_SWEEP_STEP) {
+  for (int i=SERVO_POS_MAX; i>=SERVO_POS_MIN; i-=SERVO_SWEEP_STEP) {
     pointAt(i);
   }
 
   pinMode(PIN_PING_TRIG, OUTPUT);
   pinMode(PIN_PING_ECHO, INPUT);
+
+  startLEDPulsing(true);
   // Store an initial distance reading
   dist = getDistanceCM();
-}
+};
 
 void loop() {
-  int loop_timer = millis();
+  unsigned long int loop_timer = millis();
 
+  for (int sweep=0; sweep<SWEEP_COUNT; sweep++) {
+    for (int pos=SERVO_POS_MIN; pos<SERVO_POS_MAX; pos+=SERVO_SWEEP_STEP) {
+      refreshPulsingLED();
+      pointAt(pos);
+    }
+  }
   // Guarantee a minimum time spent in each loop iteration
-  while ((millis() - loop_timer) < LOOP_MIN_TIME_MS) {
-    delay(LOOP_TIMER_STEP_MS);
+  if (millis() > loop_timer) {  // the millis() counter resets to 0
+    while ((millis() - loop_timer) < LOOP_MIN_TIME_MS) {
+      refreshPulsingLED();
+      delay(LOOP_TIMER_STEP_MS);
+    }
+  } else {
+    refreshPulsingLED();
+    delay(LOOP_TIMER_STEP_MS);  // easy guess
   }
 }
